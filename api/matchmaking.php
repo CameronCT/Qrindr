79c91ea6edaef5f62f2c44631cee8134fe1e9479
Cameron <CameronCT@users.noreply.github.com>
2019-08-05 16:47:40 -0700

    Added basic matchmaking

--- api/matchmaking.php
+++ api/matchmaking.php
@@ -21,25 +21,33 @@ $NAMES[3] = [
 
 function searchQueue($conn, $queueName, $queueRegion) {
     /* Search for Available Players */
+    $getSkillrating = $conn->prepare("SELECT m_skillrating FROM quakechampions_matchmaking WHERE m_name = ? AND m_region = ?");
+    $getSkillrating->execute(array($queueName, $queueRegion));
+    $sessionRating = (int) $getSkillrating->fetchColumn();
+
     $searchQueue = $conn->prepare("
         SELECT 
             m_id, m_name, m_alias, m_updated FROM quakechampions_matchmaking 
         WHERE 
-            m_name != ? AND m_region = ? AND m_skillrating < (m_skillrating + 100) AND m_skillrating > (m_skillrating - 400)
+            m_name != ? AND m_region = ? AND m_skillrating < ? AND m_skillrating > ?
         ORDER BY m_id DESC
         LIMIT 1
     ");
-    $searchQueue->execute(array($queueName, $queueRegion));
+    $searchQueue->execute(array($queueName, $queueRegion, ($sessionRating + 100), ($sessionRating - 400) ));
     $useMatch = $searchQueue->fetch(PDO::FETCH_ASSOC);
 
     /* Check if Match Exists */
     $matchExists = $conn->prepare("SELECT COUNT(g_id) FROM quakechampions_grindr WHERE g_player1 = ? AND g_player2 = ? OR g_player1 = ? AND g_player2 = ?");
-    $matchExists->execute(array($_SESSION['queueName'], $useMatch['m_name'], $useMatch['m_name'], $_SESSION['queueName']));
+    $matchExists->execute(array($queueName, $useMatch['m_name'], $useMatch['m_name'], $queueName));
 
-    if ($matchExists->fetchColumn() > 0)
+    /* Make Sure Opponent Doesn't Have a Match */
+    $opponentMatchExists = $conn->prepare("SELECT COUNT(g_id) AS rowCount FROM quakechampions_grindr WHERE g_player1 = ? OR g_player2 = ?");
+    $opponentMatchExists->execute(array($useMatch['m_name'], $useMatch['m_name']));
+
+    if ($matchExists->fetchColumn() == 0 && $opponentMatchExists->fetchColumn() == 0)
         return $useMatch;
-    else
-        searchQueue($conn, $queueName, $queueRegion);
+    
+    return false;
 }
 
 if (isset($_GET['stats'])) {
@@ -51,12 +59,15 @@ if (isset($_GET['stats'])) {
 
 if (isset($_GET['update'])) {
 
-    /* Remove everyone that's been inactive for more than 5 minutes */
+    /* Remove everyone that's been inactive for more than 15 seconds */
     $getQueue = $conn->query("SELECT m_id, m_name, m_updated, m_datetime FROM quakechampions_matchmaking");
     foreach ($getQueue->fetchAll(PDO::FETCH_ASSOC) as $v) {
-        if ( ($v['m_updated'] - 60) > time())
-            $removeQueue = $conn->query("DELETE FROM quakechampions_matchmaking WHERE (m_updated - 5) > NOW()");
+        if ( ($v['m_updated'] + 15) < time())
+            $removeQueue = $conn->query("DELETE FROM quakechampions_matchmaking WHERE (m_updated + 15) < NOW()");
     }
+    
+    /* Remove Generates Matchmakings that's been inactive for more than 1 minute */
+    $getMatching = $conn->query("DELETE FROM quakechampions_grindr WHERE g_datetime < (NOW() - 15)");
     
     /* if Player is inQueue == true */
     if (isset($_SESSION['inQueue']) && $_SESSION['inQueue'] == true) {
@@ -65,55 +76,68 @@ if (isset($_GET['update'])) {
         $updateQueue = $conn->prepare("UPDATE quakechampions_matchmaking SET m_updated = ? WHERE m_name = ?");
         $updateQueue->execute(array(time(), $_SESSION['queueName']));
 
-        /* Search for Available Players */
-        $getPossibleMatch = searchQueue($conn, $_SESSION['queueName'], $_SESSION['queueRegion']);
-
-        /* Start the Process of Creating a Match */
-        $queueAndMatch = $conn->prepare("
-            INSERT INTO 
-                quakechampions_grindr
-            ( g_player1, g_player2 )
-                VALUES
-            ( ?, ? )
-        ");
-        $queueAndMatch->execute(array($_SESSION['queueName'], $getPossibleMatch['m_name']));
-
         /* if Match Exists */
-        $findMatch = $conn->prepare("SELECT COUNT(g_id) AS rowCount, g_player1, g_player2 FROM quakechampions_grindr WHERE g_player1 = ? OR  g_player2 = ?");
+        $findMatch = $conn->prepare("SELECT COUNT(g_id) AS rowCount, g_player1, g_player2, g_serial FROM quakechampions_grindr WHERE g_player1 = ? OR g_player2 = ?");
         $findMatch->execute(array($_SESSION['queueName'], $_SESSION['queueName']));
         $findMatched = $findMatch->fetch(PDO::FETCH_ASSOC);
 
-        if ($findMatched['rowCount'] > 0) {
-            $getMatchURL = $conn->prepare("SELECT m_uuid, m_passphrase FROM quakechampions WHERE player1 = ? AND player2 = ? OR player1 = ? AND player2 = ?");
-            $getMatchURL->execute(array($findMatched['g_player1'], $findMatched['g_player2'], $findMatched['g_player2'], $findMatched['g_player1']));
+        if ($findMatched['rowCount'] == 1) {
+
+            $getMatchURL = $conn->prepare("SELECT uuid, `password` FROM quakechampions WHERE uuid = ? AND player1 = ? AND player2 = ? OR uuid = ? AND player1 = ? AND player2 = ?");
+            $getMatchURL->execute(array(md5($findMatched['g_serial']), $findMatched['g_player1'], $findMatched['g_player2'], md5($findMatched['g_serial']), $findMatched['g_player2'], $findMatched['g_player1']));
             $getMatchedURL = $getMatchURL->fetch(PDO::FETCH_ASSOC);
 
-            // Create Match URL
-            $redirect = $getMatchedURL['m_uuid'] . '/' . md5($getMatchedURL['m_passphrase']) . '/' . $_SESSION['queueName'];
+            // Check if a Match has been found && Create Match URL
+            if ($getMatchedURL['uuid']) {
+                $redirect = $getMatchedURL['uuid'] . '/' . $getMatchedURL['password'] . '/' . $_SESSION['queueName'];
+                $msg = "A new match has been found! (" . filter_var($findMatched['g_player1'], FILTER_SANITIZE_STRING) . " vs " . filter_var($findMatched['g_player2'], FILTER_SANITIZE_STRING) . ")";
 
-            // Reset Queue Stats
-            unset($_SESSION['queueName']);
-            unset($_SESSION['queueRegion']);
-            unset($_SESSION['inQueue']);
+                /* Unset Session Variables */
+                unset($_SESSION['queueName']);
+                unset($_SESSION['queueRegion']);
+                unset($_SESSION['inQueue']);
+                
+            }
+        } elseif ($findMatched['rowCount'] == 0) { 
+
+            /* Search for Available Players */
+            $getPossibleMatch = searchQueue($conn, $_SESSION['queueName'], $_SESSION['queueRegion']);
+
+            if ($getPossibleMatch != false) {
+                /* Start the Process of Creating a Match */
+                $serial = rand(1, 999999);
+
+                $queueAndMatch = $conn->prepare("
+                    INSERT INTO 
+                        quakechampions_grindr
+                    ( g_player1, g_player2, g_serial )
+                        VALUES
+                    ( ?, ?, ? );
+
+                    INSERT INTO
+                        quakechampions
+                    ( uuid, player1, player2, bestof, password )
+                        VALUES
+                    ( ?, ?, ?, ?, ? );
+                ");
+                $queueAndMatch->execute(array($_SESSION['queueName'], $getPossibleMatch['m_name'], $serial, md5($serial), $_SESSION['queueName'], $getPossibleMatch['m_name'], 3, md5(rand(1, 999999)) ));
+            } 
         }
-
     }
 
 } 
 
 if (isset($_POST['remove'])) {
 
-    if (isset($_SESSION['queueName']) && $_SESSION['queueRegion']) {
+    if (isset($_SESSION['queueName']) && isset($_SESSION['queueRegion'])) {
         $removeName = $conn->prepare("DELETE FROM quakechampions_matchmaking WHERE m_name = ? AND m_region = ?");
         $removeName->execute(array($_SESSION['queueName'], $_SESSION['queueRegion']));
 
-        if ($removeName->rowCount() > 0) {
-            unset($_SESSION['queueName']);
-            unset($_SESSION['queueRegion']);
-            unset($_SESSION['inQueue']);
+        unset($_SESSION['queueName']);
+        unset($_SESSION['queueRegion']);
+        unset($_SESSION['inQueue']);
 
-            $msg = "You have successfully been removed from the queue!";
-        }
+        $msg = "You have successfully been removed from the queue!";
     }
 
 }
@@ -123,11 +147,9 @@ if (isset($_POST['add']) && isset($_POST['region'])) {
     $_POST['add'] = filter_var($_POST['add'], FILTER_SANITIZE_STRING);
 
     /* Check if Name Exists */
-    if (isset($_SESSION['inQueue']) && $_SESSION['inQueue'] == true) {
-        $checkName = $conn->prepare("SELECT COUNT(m_id) FROM quakechampions_matchmaking WHERE m_name = ?");
-        $checkName->execute(array($_POST['add']));
-        if ($checkName->fetchColumn() > 0) $err = "This name is already in queue!";
-    }
+    $checkName = $conn->prepare("SELECT COUNT(m_id) FROM quakechampions_matchmaking WHERE m_name = ?");
+    $checkName->execute(array($_POST['add']));
+    if ($checkName->fetchColumn() > 0) $err = "Cooldown! You must wait 15 seconds before trying to queue again!";
 
     /* Check if QC Name Exists */
     $apiurl = @file_get_contents($qcapi . urlencode($_POST['add']));
@@ -144,11 +166,19 @@ if (isset($_POST['add']) && isset($_POST['region'])) {
 
     if (!$err || $err == null) {
 
+        $skillRating = 1350;
+
         $alias = $NAMES[1][rand(0, count($NAMES[1]) - 1)] . $NAMES[2][rand(0, count($NAMES[2]) - 1)] . $NAMES[3][rand(0, count($NAMES[3]) - 1)];
 
+        /* Skillrating, make highest Skill Rating max out at 2200 */
+        if ($res['playerRatings']['duel']['rating'] > 2300)
+            $skillRating = 2300;
+        else
+            $skillRating = ($res['playerRatings']['duel']['rating'] + $res['playerRatings']['duel']['deviation']);
+
         /* Add Name to Queue */
-        $addName = $conn->prepare("INSERT INTO quakechampions_matchmaking ( m_name, m_alias, m_region, m_skillrating ) VALUES ( ?, ?, ?, ? )");
-        $addName->execute(array($_POST['add'], $alias, $_POST['region'], ($res['playerRatings']['duel']['rating'] + $res['playerRatings']['duel']['deviation'])));
+        $addName = $conn->prepare("INSERT INTO quakechampions_matchmaking ( m_name, m_alias, m_region, m_updated, m_skillrating ) VALUES ( ?, ?, ?, ?, ? )");
+        $addName->execute(array($_POST['add'], $alias, $_POST['region'], time(), $skillRating));
 
         if ($addName->rowCount() > 0) {
             $_COOKIE['savedName'] = $_POST['add'];
