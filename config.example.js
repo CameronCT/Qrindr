14f41bacdf586d2498007e6f192b9a67b26c27d7
Cameron <CameronCT@users.noreply.github.com>
2020-06-07 18:45:21 -0700

    add: example config

---
+++ config.example.js
@@ -0,0 +1,17 @@
+module.exports = {
+    'HttpPort': 8000,
+
+    'OAUTH': {
+        'Discord': {
+            'Client': '',
+            'Secret': '',
+        }
+    },
+    'MariaDB': {
+        'Host': 'localhost',
+        'Name': 'qrindr',
+        'User': 'root',
+        'Pass': ''
+    },
+
+};
