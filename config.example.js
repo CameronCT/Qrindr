b62332f07a25f3754e73713af96bae8db11d299c
Cameron <CameronCT@users.noreply.github.com>
2020-06-09 10:41:25 -0700

    added base_url/api_url call

--- config.example.js
+++ config.example.js
@@ -4,6 +4,9 @@ module.exports = {
     'Session': { secret: 'my_secret_key' },
     'JWT': { 'key': 'my_secret_jwt_key', 'expiryMin': ((24 * (60 * 60)) * 7), 'expiryMax': ((24 * (60 * 60)) * 30) },
 
+    'BASE_URL': 'localhost:3000',
+    'API_URL': 'localhost:8000',
+
     'OAUTH': {
         'Discord': {
             'Client': '',
