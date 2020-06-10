eaf7241811bc822c262ab4fa8e017b8b244bc2dc
Cameron <CameronCT@users.noreply.github.com>
2020-06-10 09:08:52 -0700

    Added labels for input fields

--- public/src/components/pages/home/matches/CreateMatch.tsx
+++ public/src/components/pages/home/matches/CreateMatch.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Games from "./Games";
 
 class CreateMatch extends Component {
 
@@ -7,29 +8,32 @@ class CreateMatch extends Component {
             <form method="post">
                 <div className="bg-gray-800 text-gray-400 shadow p-6">
                     <h1 className="pb-4 text-2xl text-gray-300 text-center font-semibold">Configuration</h1>
-                    <select name="gameId" className="mb-4">
-                        <option value="0">Quake Champions - Best of 3</option>
-                        <option value="1">Quake Champions - Best of 5</option>
-                        <option value="2">Diabotical Duel - Best of 3</option>
-                        <option value="3">Diabotical Duel - Best of 5</option>
-                        <option value="4">Diabotical Duel - Best of 7</option>
-                        <option value="5">Diabotical MacGuffin - Best of 3</option>
-                        <option value="6">Diabotical MacGuffin - Best of 5</option>
-                        <option value="7">Diabotical MacGuffin - Best of 7</option>
-                    </select>
+                    <div className="text-gray-200 font-semibold">Game</div>
+                    <Games />
+
+                    <div className="text-gray-200 font-semibold">Format</div>
                     <select name="gameFormat" className="mb-4">
                         <option value="0">Practice</option>
                         <option value="1">Tournament</option>
                     </select>
+
                     <div className="border-t border-gray-700 my-4"></div>
+
                     <h1 className="pb-4 text-2xl text-gray-300 text-center font-semibold">Information</h1>
+                    <div className="text-gray-200 font-semibold">You</div>
                     <input type="text" name="player1" placeholder="Liquid rapha" className="mb-4" required />
+
+                    <div className="text-gray-200 font-semibold">Opponent</div>
                     <input type="text" name="player2" placeholder="Nemesis dooi" className="mb-4" required />
+
+                    <div className="text-gray-200 font-semibold">Cointoss</div>
                     <select name="cointoss" className="mb-4">
                         <option value="0">Random</option>
                         <option value="1">You</option>
                         <option value="2">Opponent</option>
                     </select>
+
+                    <div className="text-gray-200 font-semibold">Secret</div>
                     <input type="password" name="secret" placeholder="********" className="mb-4" required />
                 </div>
 
