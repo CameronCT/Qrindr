74c6c131613719fe93e69a94f8804e0c2afa3afc
Cameron <CameronCT@users.noreply.github.com>
2020-06-09 22:51:33 -0700

    Update Navigation.tsx

--- public/src/components/layout/Navigation.tsx
+++ public/src/components/layout/Navigation.tsx
@@ -13,13 +13,13 @@ class Navigation extends Component {
                             Qrindr
                         </div>
                         <div className="w-4/5 text-right my-auto">
-                            <Link className="text-gray-300 font-semibold uppercase pl-3" to="/">Home</Link>
-                            <Link className="text-gray-300 font-semibold uppercase pl-3" to="/about">About</Link>
-                            <a href="https://discord.gg/BAEkm58" className="text-gray-300 font-semibold uppercase pl-3">Discord</a>
-                            <a href="https://github.com/CameronCT/Qrindr" className="text-gray-300 font-semibold uppercase pl-3">GitHub</a>
-                            <a href="https://streamlabs.com/gnikgg" className="text-gray-300 font-semibold uppercase pl-3">Donate</a>
+                            <Link className="text-gray-300 font-semibold pl-3" to="/">Home</Link>
+                            <Link className="text-gray-300 font-semibold pl-3" to="/about">About</Link>
+                            <a href="https://discord.gg/BAEkm58" className="text-gray-300 font-semibold pl-3">Discord</a>
+                            <a href="https://github.com/CameronCT/Qrindr" className="text-gray-300 font-semibold pl-3">GitHub</a>
+                            <a href="https://streamlabs.com/gnikgg" className="text-gray-300 font-semibold pl-3">Donate</a>
                             <span className="border-r border-gray-500 px-2"></span>
-                            <a href="#home" className="text-gray-300 font-semibold uppercase pl-3">Settings</a>
+                            <a href="#home" className="text-gray-300 font-semibold pl-3">Settings</a>
                         </div>
                     </div>
                 </div>
