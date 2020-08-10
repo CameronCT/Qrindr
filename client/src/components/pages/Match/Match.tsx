ef41447036e7f19533bb891b4dee3f2543f24741
Cameron <CameronCT@users.noreply.github.com>
2020-08-10 16:01:47 -0700

    feat: match Cointoss and Config information

--- client/src/components/pages/Match/Match.tsx
+++ client/src/components/pages/Match/Match.tsx
@@ -24,6 +24,10 @@ class Match extends Component {
                 <div className={"flex flex-wrap border-b-2 border-indigo-700 mb-4 pb-4 mx-2"}>
                     <div className={"w-full md:w-3/4 text-xl my-auto text-white"}>
                         <span className={"font-semibold"}>{data.matchPlayerOne}</span> vs <span className={"font-semibold"}>{data.matchPlayerTwo}</span>
+                        <div className={"text-xs text-gray-600"}>
+                            {data.matchConfig} <span className={"px-2"}>|</span>
+                            {data.matchCointoss == 0 ? 'Random' : 'Manual'} Cointoss
+                        </div>
                     </div>
                     <div className={"w-full md:w-1/4 text-center md:text-right my-auto"}>
                         <a href="/" className={"ml-2 px-4 py-2 font-semibold text-white bg-indigo-500 hover:bg-indigo-600 rounded-full"}>
