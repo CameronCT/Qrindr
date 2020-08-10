f30ed78cecff95f4f144e682fd9880a9a6e67d4d
Cameron <CameronCT@users.noreply.github.com>
2020-08-10 16:30:58 -0700

    feat: used player name from url to match to validate user

--- client/src/components/pages/Match/Veto.tsx
+++ client/src/components/pages/Match/Veto.tsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCircleNotch} from "@fortawesome/pro-duotone-svg-icons";
+import {faCheck, faTimes} from "@fortawesome/pro-solid-svg-icons";
 
 interface IProps {
+    currentName: string | boolean;
     name: string;
     type: string;
     value: any;
@@ -15,9 +17,10 @@ class Veto extends Component<IProps> {
 
     render() {
 
-        const { name, value, next, maps, champions } = this.props;
+        const { currentName, name, value, next, maps, champions } = this.props;
         let { type } = this.props;
         let css = 'yellow';
+        let icon = { icon: faCircleNotch, color: 'text-gray-300' };
 
         if (!type)
             type = 'map_pick';
@@ -36,9 +39,11 @@ class Veto extends Component<IProps> {
         switch (types[1]) {
             case 'ban':
                 css = 'red';
+                icon = { icon: faTimes, color: 'text-red-500' };
                 break;
             case 'pick':
                 css = 'green';
+                icon = { icon: faCheck, color: 'text-green-500' };
                 break;
             default:
                 css = 'orange';
@@ -46,11 +51,16 @@ class Veto extends Component<IProps> {
 
         return !next ? (
             <div className={`border-2 border-${css}-800 bg-gray-800 p-3 text-white shadow-md mb-4`}>
-                <FontAwesomeIcon icon={faCircleNotch} spin /> <span className="font-semibold">{name}</span> has {types[1] == 'ban' ? 'banned' : 'picked'} <span className="font-semibold">{types[0] === 'map' ? maps[value] : champions[value]}</span>.
+                <FontAwesomeIcon icon={icon.icon} className={icon.color} /> <span className="font-semibold">{name}</span> has {types[1] == 'ban' ? 'banned' : 'picked'} <span className="font-semibold">{types[0] === 'map' ? maps[value] : champions[value]}</span>.
             </div>
         ) : (
             <div className={`border-2 border-${css}-800 bg-gray-800 p-3 text-white shadow-md mb-4`}>
                 <FontAwesomeIcon icon={faCircleNotch} spin /> Waiting on <span className="font-semibold">{name}</span> to {types[1]} a {types[0]}.
+                {name === currentName && (
+                  <div>
+                    Pick
+                  </div>
+                )}
             </div>
         )
 
