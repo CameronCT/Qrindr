aaa97d645bee4e06bf0100af27a8266d6143cdca
Cameron <CameronCT@users.noreply.github.com>
2020-08-09 23:50:29 -0700

    feat: match, testing ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from "./components/pages/Home/Home";
 import NavigationDesktop from "./components/navigation/NavigationDesktop";
+import Match from "./components/pages/Match/Match";
 
 export const App = () => {
   return (
@@ -11,6 +12,7 @@ export const App = () => {
             <div className="p-4 md:p-6 lg:p-12 w-full">
                 <Switch>
                     <Route exact path="/" component={Home} />
+                    <Route path="/match" component={Match} />
                 </Switch>
             </div>
         </div>
--- client/src/components/pages/Match/Match.tsx
+++ client/src/components/pages/Match/Match.tsx
@@ -1,31 +1,41 @@
 import React, { Component } from 'react';
+import data from './data.json';
+import Veto from "./Veto";
 
 class Match extends Component {
-
-    data = [
-        {
-            matchId: 0,
-            matchConfig: 'Quake Champions - Timelimit Duel (Best of 3)',
-            matchPlayerOne: 'GNiK',
-            matchPlayerTwo: 'rapha',
-            matchCointoss: 0,
-            matchSteps: [
-                {
-                    stepType: 'map_pick',
-                    stepValue: 0,
-                }
-            ]
-        }
-    ];
+    state = {
+        data: {} as any
+    };
 
     componentDidMount() {
-
+        this.setState({ data });
     }
 
+    /*
+    {data.matchSteps.list.map((key, value: any) => (
+                            <div>
+                                <Veto name={"playerNameHere"} type={value} value={data.matchSteps.values[key]} />
+                            </div>
+                        ))}
+     */
     render() {
         return (
             <div>
-                Test
+                <div className={"flex flex-wrap"}>
+                    <div className="w-2/5 bg-red-500 min-h-screen">
+                        {data.matchSteps.list.map((key, value: any) => (
+                            <div>
+                                <Veto name={"playerNameHere"} type={value} value={data.matchSteps.values[key]} />
+                            </div>
+                        ))}
+                    </div>
+                    <div className="w-2/5 bg-blue-800">
+
+                    </div>
+                    <div className="w-1/5 bg-yellow-900">
+
+                    </div>
+                </div>
             </div>
         )
     }
