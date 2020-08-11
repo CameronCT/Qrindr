9da4c0c329acd56884ebda5d48fe655ed0362265
Cameron <CameronCT@users.noreply.github.com>
2020-08-10 22:48:16 -0700

    feat: Cointoss options

---
+++ server/Data/Options/Cointoss.php
@@ -0,0 +1,16 @@
+<?php
+
+$cointoss = [
+    0 => [
+        'cointossId' => 0,
+        'cointossName' => 'Random',
+    ],
+    1 => [
+        'cointossId' => 1,
+        'cointossName' => 'You',
+    ],
+    2 => [
+        'cointossId' => 2,
+        'cointossName' => 'Opponent',
+    ]
+];
