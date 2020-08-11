9818130c7dbf4121c3be669daa0706c255960893
Cameron <CameronCT@users.noreply.github.com>
2020-08-10 22:48:10 -0700

    feat: Game Configs

---
+++ server/Data/Games.php
@@ -0,0 +1,14 @@
+<?php
+
+$games = [
+    0 => [
+        'configId'   => 0,
+        'configName' => 'Quake Champions - Timelimit Duel (Best of 3)',
+        'configFile' => 'QuakeChampions_TimelimitDuel_BO3'
+    ],
+    1 => [
+        'configId'   => 1,
+        'configName' => 'Quake Champions - Timelimit Duel (Best of 5)',
+        'configFile' => 'QuakeChampions_TimelimitDuel_BO5'
+    ],
+];
