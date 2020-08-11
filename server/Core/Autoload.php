1fc6381622c0b3c8aba5147adb652c75c1c27e1f
Cameron <CameronCT@users.noreply.github.com>
2020-08-10 23:48:41 -0700

    chore: added development parameter for proxy requests

--- server/Core/Autoload.php
+++ server/Core/Autoload.php
@@ -4,7 +4,7 @@ $baseDir = dirname(__DIR__);
 
 require_once('Database.php');
 
-if ($_SERVER['REMOTE_ADDR'] == '::1')
+if ($_SERVER['REMOTE_ADDR'] == '::1' || $_SERVER['REMOTE_ADDR'] == '127.0.0.1')
     require_once($baseDir . '/Configs/Development.php');
 else
     require_once($baseDir . '/Configs/Production.php');
