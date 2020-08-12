2da09beb59b4df8260a7d517a62266a5acb17abc
Cameron <CameronCT@users.noreply.github.com>
2020-08-11 23:03:52 -0700

    fix: Step PHP errors

--- server/Step.php
+++ server/Step.php
@@ -3,23 +3,25 @@
 $err = "";
 $msg = "";
 
-if (!isset($_POST['secret']) || !isset($_POST['player']) || !isset($_POST['value'])
+if (!isset($_POST['secret']) || !isset($_POST['player']) || !isset($_POST['value']))
     exit;
 
 $matchData = $conn->getDataFromMatchHash($_POST['hash']);
 
 if ($_POST['secret'] != $matchData['matchSecret'])
-    exit;
+    $err = "Invalid match secret!";
 
-if ($matchData['playerOne'] != $_POST['player'] && $matchData['playerTwo'] != $_POST['player'])
-    exit;
+if ($matchData['matchPlayerOne'] != $_POST['player'] && $matchData['matchPlayerTwo'] != $_POST['player'])
+    $err = "Invalid player name!";
 
-$addStep = $conn->addStepToMatch((int) $matchData['matchId'], (int) $_POST['value']));
-if ($addStep)
-    $msg = "Step added successfully";
-else
-    $err = "Unable to add step";
+if ($err == "") {
+    $addStep = $conn->addStepToMatch($matchData['matchId'], $_POST['value']);
+    if ($addStep)
+        $msg = "Step added successfully";
+    else
+        $err = "Unable to add step";
+}
 
 header('Content-Type: application/json');
-echo json_encode([ error => $err, success => $msg ], true);
+echo json_encode([ 'error' => $err, 'success' => $msg ], true);
 exit;
