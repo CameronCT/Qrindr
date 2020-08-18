290db3842b50ea1c973e857c64332a647cf8e344
Cameron <CameronCT@users.noreply.github.com>
2020-08-17 17:56:18 -0700

    chore: updated functions for optional headers

--- server/Core/Functions.php
+++ server/Core/Functions.php
@@ -1,8 +1,9 @@
 <?php
 
-function curl_get_content(String $url){
+function curl_get_content(String $url, $headers = []){
       $ch = curl_init();
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
       curl_setopt($ch, CURLOPT_USERAGENT, "API_GITHUB_FETCHER" );
       curl_setopt($ch, CURLOPT_URL, $url);
       $data = curl_exec($ch);
