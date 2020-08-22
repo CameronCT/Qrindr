5f228d822ce3cecfb9dc40c1502a4ec1c29e9eb7
Cameron <CameronCT@users.noreply.github.com>
2020-08-21 21:06:11 -0700

    chore: added Quake Live TDM and CTF modes

--- server/Data/Games.php
+++ server/Data/Games.php
@@ -13,12 +13,12 @@ $games = [
     ],
     2 => [
         'configId'   => 2,
-        'configName' => 'Diabotical - Esports Duel (Best of 3)',
+        'configName' => 'Diabotical - Casual Duel (Best of 3)',
         'configFile' => 'Diabotical_Duel_BO3'
     ],
     3 => [
         'configId'   => 3,
-        'configName' => 'Diabotical - Esports Duel (Best of 5)',
+        'configName' => 'Diabotical - Casual Duel (Best of 5)',
         'configFile' => 'Diabotical_Duel_BO5'
     ],
     4 => [
@@ -30,5 +30,35 @@ $games = [
         'configId'   => 5,
         'configName' => 'Quake Live Duel (Best of 5)',
         'configFile' => 'QuakeLive_Duel_BO5'
+    ],
+    6 => [
+        'configId'   => 6,
+        'configName' => 'Quake Live CTF (Best of 1)',
+        'configFile' => 'QuakeLive_CTF_BO1'
+    ],
+    7 => [
+        'configId'   => 7,
+        'configName' => 'Quake Live CTF (Best of 3)',
+        'configFile' => 'QuakeLive_CTF_BO3'
+    ],
+    8 => [
+        'configId'   => 8,
+        'configName' => 'Quake Live CTF (Best of 5)',
+        'configFile' => 'QuakeLive_CTF_BO5'
+    ],
+    9 => [
+        'configId'   => 9,
+        'configName' => 'Quake Live TDM (Best of 1)',
+        'configFile' => 'QuakeLive_TDM_BO1'
+    ],
+    10 => [
+        'configId'   => 10,
+        'configName' => 'Quake Live TDM (Best of 3)',
+        'configFile' => 'QuakeLive_TDM_BO3'
+    ],
+    11 => [
+        'configId'   => 11,
+        'configName' => 'Quake Live TDM (Best of 5)',
+        'configFile' => 'QuakeLive_TDM_BO5'
     ],
 ];
