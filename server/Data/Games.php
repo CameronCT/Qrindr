d1b4d0b2784a23d506166ac4a7d1f2bd9e42d221
Cameron <CameronCT@users.noreply.github.com>
2020-08-21 21:25:51 -0700

    feat: Quake Champions 2V2 Best of 3/5

--- server/Data/Games.php
+++ server/Data/Games.php
@@ -1,64 +1,82 @@
 <?php
 
-$games = [
-    0 => [
-        'configId'   => 0,
-        'configName' => 'Quake Champions - Timelimit Duel (Best of 3)',
-        'configFile' => 'QuakeChampions_TimelimitDuel_BO3'
-    ],
-    1 => [
-        'configId'   => 1,
-        'configName' => 'Quake Champions - Timelimit Duel (Best of 5)',
-        'configFile' => 'QuakeChampions_TimelimitDuel_BO5'
-    ],
-    2 => [
-        'configId'   => 2,
-        'configName' => 'Diabotical - Casual Duel (Best of 3)',
-        'configFile' => 'Diabotical_Duel_BO3'
-    ],
-    3 => [
-        'configId'   => 3,
-        'configName' => 'Diabotical - Casual Duel (Best of 5)',
-        'configFile' => 'Diabotical_Duel_BO5'
-    ],
-    4 => [
-        'configId'   => 4,
-        'configName' => 'Quake Live Duel (Best of 3)',
-        'configFile' => 'QuakeLive_Duel_BO3'
-    ],
-    5 => [
-        'configId'   => 5,
-        'configName' => 'Quake Live Duel (Best of 5)',
-        'configFile' => 'QuakeLive_Duel_BO5'
-    ],
-    6 => [
-        'configId'   => 6,
-        'configName' => 'Quake Live CTF (Best of 1)',
-        'configFile' => 'QuakeLive_CTF_BO1'
-    ],
-    7 => [
-        'configId'   => 7,
-        'configName' => 'Quake Live CTF (Best of 3)',
-        'configFile' => 'QuakeLive_CTF_BO3'
-    ],
-    8 => [
-        'configId'   => 8,
-        'configName' => 'Quake Live CTF (Best of 5)',
-        'configFile' => 'QuakeLive_CTF_BO5'
-    ],
-    9 => [
-        'configId'   => 9,
-        'configName' => 'Quake Live TDM (Best of 1)',
-        'configFile' => 'QuakeLive_TDM_BO1'
-    ],
-    10 => [
-        'configId'   => 10,
-        'configName' => 'Quake Live TDM (Best of 3)',
-        'configFile' => 'QuakeLive_TDM_BO3'
-    ],
-    11 => [
-        'configId'   => 11,
-        'configName' => 'Quake Live TDM (Best of 5)',
-        'configFile' => 'QuakeLive_TDM_BO5'
-    ],
+$games[] = [
+    'configId'   => 0,
+    'configName' => 'Quake Champions - Timelimit Duel (Best of 3)',
+    'configFile' => 'QuakeChampions_TimelimitDuel_BO3'
+];
+
+$games[] = [
+    'configId'   => 1,
+    'configName' => 'Quake Champions - Timelimit Duel (Best of 5)',
+    'configFile' => 'QuakeChampions_TimelimitDuel_BO5'
+];
+
+$games[] = [
+    'configId'   => 2,
+    'configName' => 'Quake Champions - 2V2 TDM (Best of 3)',
+    'configFile' => 'QuakeChampions_2V2TDM_BO3'
+];
+
+$games[] = [
+    'configId'   => 3,
+    'configName' => 'Quake Champions - 2V2 TDM (Best of 5)',
+    'configFile' => 'QuakeChampions_2V2TDM_BO5'
+];
+
+$games[] = [
+    'configId'   => 4,
+    'configName' => 'Diabotical - Casual Duel (Best of 3)',
+    'configFile' => 'Diabotical_Duel_BO3'
+];
+$games[] = [
+    'configId'   => 5,
+    'configName' => 'Diabotical - Casual Duel (Best of 5)',
+    'configFile' => 'Diabotical_Duel_BO5'
+];
+
+$games[] = [
+    'configId'   => 6,
+    'configName' => 'Quake Live Duel (Best of 3)',
+    'configFile' => 'QuakeLive_Duel_BO3'
+];
+$games[] = [
+    'configId'   => 7,
+    'configName' => 'Quake Live Duel (Best of 5)',
+    'configFile' => 'QuakeLive_Duel_BO5'
+];
+
+$games[] = [
+    'configId'   => 8,
+    'configName' => 'Quake Live CTF (Best of 1)',
+    'configFile' => 'QuakeLive_CTF_BO1'
+];
+
+$games[] = [
+    'configId'   => 9,
+    'configName' => 'Quake Live CTF (Best of 3)',
+    'configFile' => 'QuakeLive_CTF_BO3'
+];
+$games[] = [
+    'configId'   => 10,
+    'configName' => 'Quake Live CTF (Best of 5)',
+    'configFile' => 'QuakeLive_CTF_BO5'
+];
+
+$games[] = [
+    'configId'   => 11,
+    'configName' => 'Quake Live TDM (Best of 1)',
+    'configFile' => 'QuakeLive_TDM_BO1'
+];
+
+$games[] = [
+    'configId'   => 12,
+    'configName' => 'Quake Live TDM (Best of 3)',
+    'configFile' => 'QuakeLive_TDM_BO3'
+];
+
+$games[] = [
+    'configId'   => 13,
+    'configName' => 'Quake Live TDM (Best of 5)',
+    'configFile' => 'QuakeLive_TDM_BO5'
 ];
---
+++ server/Data/Games/QuakeChampions_2V2TDM_BO5.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * matchSplit*** = Length is with 0 counting as 1
+ * matchSteps.Next = Length without 0 counting as 1
+ */
+
+$game['matchConfig']                = 'Quake Champions 2V2 TDM (Best of 5)';
+$game['matchSplitMapOne']           = 999;
+$game['matchSplitMapTwo']           = 999;
+$game['matchSplitMapThree']         = 999;
+$game['matchSplitMapFour']          = 999;
+$game['matchSplitMapFive']          = 999;
+$game['matchSplitMapSix']           = 999;
+$game['matchSplitMapSeven']         = 999;
+$game['matchTimeout']               = 30;
+$game['matchSteps']                 = [
+    'next' => 0,
+    'list' => [
+          'map_pick',
+          'map_pick',
+          'map_pick',
+          'map_pick',
+          'map_ban',
+          'map_ban',
+          'map_pick',
+    ],
+    'player' => [
+        0,
+        1,
+        0,
+        1,
+        0,
+        1,
+        0
+    ],
+    'values' => []
+];
+$game['matchMaps'] = [
+    'available' => [0, 1, 2, 3, 4, 5, 6],
+    'taken' => [],
+    'picked' => [],
+    'list' => [
+        'Awoken',
+        'Blood Covenant',
+        'Tempest Shrine',
+        'Corrupted Keep',
+        'Molten Falls',
+        'Ruins of Sarnath',
+        'Vale of Pnath'
+    ],
+    'listAbbreviation' => [
+        'Awoken',
+        'BC/DM6',
+        'Tempest',
+        'CK',
+        'Molten',
+        'Ruins',
+        'Vale'
+    ],
+    'listImage' => [],
+];
+$game['matchChampions'] = [
+    'available' => [],
+    'taken' => [],
+    'list' => [],
+    'listAbbreviation' => [],
+    'listImage' => [],
+];
