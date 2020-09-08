ee21865b62e37c76c1d880c075493aa79dcce957
Cameron <CameronCT@users.noreply.github.com>
2020-09-08 12:00:31 -0700

    fix: deathknight not showing up

--- server/Data/Games/QuakeChampions_TimelimitDuel_BO3.php
+++ server/Data/Games/QuakeChampions_TimelimitDuel_BO3.php
@@ -87,7 +87,7 @@ $game['matchMaps'] = [
     ]
 ];
 $game['matchChampions'] = [
-    'available' => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+    'available' => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15],
     'taken' => [],
     'list' => [
         'Nyx',
