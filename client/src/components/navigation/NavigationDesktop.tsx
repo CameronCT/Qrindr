b6378086b6e3a639001fe042b47273094ce21939
Cameron <CameronCT@users.noreply.github.com>
2020-11-08 21:32:39 -0800

    feat: redesigned navigation

--- client/src/components/navigation/NavigationDesktop.tsx
+++ client/src/components/navigation/NavigationDesktop.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faHeart, faHome, faInfoCircle} from "@fortawesome/pro-solid-svg-icons";
+import {faGamepadAlt, faHome, faInfoCircle} from "@fortawesome/pro-solid-svg-icons";
 import {faDiscord, faGithub} from "@fortawesome/free-brands-svg-icons";
 import {faDollarSign} from "@fortawesome/pro-duotone-svg-icons";
 import {Link} from "react-router-dom";
@@ -13,7 +13,15 @@ class NavigationDesktop extends Component {
             location: '/',
             icon: {
                 fa: faHome,
-                css: 'text-red-400',
+                css: 'text-green-500',
+            }
+        },
+        {
+            name: 'Create',
+            location: '/create',
+            icon: {
+                fa: faGamepadAlt,
+                css: 'text-yellow-500',
             }
         },
         {
@@ -21,7 +29,7 @@ class NavigationDesktop extends Component {
             location: '/about',
             icon: {
                 fa: faInfoCircle,
-                css: 'text-teal-400',
+                css: 'text-blue-300',
             }
         }
     ];
@@ -32,7 +40,7 @@ class NavigationDesktop extends Component {
             location: 'https://discord.gg/BAEkm58',
             icon: {
                 fa: faDiscord,
-                css: 'text-indigo-300',
+                css: 'text-indigo-400',
             }
         },
         {
@@ -49,48 +57,59 @@ class NavigationDesktop extends Component {
             location: 'https://streamlabs.com/gnikgg',
             icon: {
                 fa: faDollarSign,
-                css: 'text-green-400',
+                css: 'text-orange-400',
             }
         },
     ];
 
     render() {
         return (
-            <div className="w-12 lg:w-2/12 border-r border-gray-800" style={{ background: 'rgba(0, 0, 0, 0.3)' }}>
-                <div className="text-center lg:p-6">
-                    <div className="text-sm lg:text-3xl text-white font-bold">Qrindr</div>
-                </div>
-                {this.navOptions.map((row, key:number) => (
-                    <Link key={key} to={row.location} className="flex py-3 hover:bg-gray-900">
-                        <div className="w-full lg:w-3/12 text-center my-auto">
-                            <FontAwesomeIcon icon={row.icon.fa} className={` ${row.icon.css}`} />
+            <div>
+                <div className={"hidden lg:block"}>
+                    <div className="flex flex-wrap px-5" style={{ background: 'rgba(0, 0, 0, 0.3)' }}>
+                        <div className="w-24 mr-auto py-2">
+                            <Link to={"/"} className="text-2xl text-white font-bold">
+                                Q<span className={"hidden lg:inline"}>rindr</span>
+                            </Link>
                         </div>
-                        <div className="hidden lg:block lg:w-9/12">
-                            <div className={"font-semibold tracking-wide text-gray-100"}>
+                        {this.navOptions.map((row, key:number) => (
+                            <Link key={key} to={row.location} className="animation-short w-auto px-5 py-4 flex justify-center font-semibold text-sm text-white hover:text-red-400 uppercase hover:bg-black hover:bg-opacity-25">
                                 {row.name}
-                            </div>
-                        </div>
-                    </Link>
-                ))}
-                {this.navExternal.map((row, key: number) => (
-                    <a key={key} href={row.location} className="flex py-3 hover:bg-gray-900">
-                        <div className="w-full lg:w-3/12 text-center my-auto">
-                            <FontAwesomeIcon icon={row.icon.fa} className={` ${row.icon.css}`} />
-                        </div>
-                        <div className="hidden lg:block lg:w-9/12">
-                            <div className={"font-semibold tracking-wide text-gray-100"}>
+                            </Link>
+                        ))}
+                        {this.navExternal.map((row, key: number) => (
+                            <a key={key} href={row.location} className="animation-short w-auto px-5 py-4 flex justify-center font-semibold text-sm text-white hover:text-red-400 uppercase hover:bg-black hover:bg-opacity-25">
                                 {row.name}
-                            </div>
-                        </div>
-                    </a>
-                ))}
-                <div className={"border-t border-gray-900 py-6 text-gray-400 text-center font-semibold text-xs"}>
-                    <div>
-                        &copy; Qrindr 2020
+                            </a>
+                        ))}
                     </div>
-                    Crafted with <FontAwesomeIcon className="text-red-500" icon={faHeart} /> by GNiK
+                </div>
+                <div className={"z-50 fixed bottom-0 w-full lg:hidden"}>
+                    <div className="flex flex-wrap bg-gray-900 border-t border-gray-800">
+                        {this.navOptions.map((row, key:number) => (
+                            <Link key={key} to={row.location} className="animation-short w-1/6 py-2 text-xs uppercase hover:bg-black hover:bg-opacity-25">
+                                <div className={"text-center"}>
+                                    <FontAwesomeIcon icon={row.icon.fa} className={`text-xl ${row.icon.css}`} />
+                                    <div className={"text-white font-semibold"}>
+                                        {row.name}
+                                    </div>
+                                </div>
+                            </Link>
+                        ))}
+                        {this.navExternal.map((row, key: number) => (
+                            <a key={key} href={row.location} className="animation-short w-1/6 py-2 text-xs uppercase hover:bg-black hover:bg-opacity-25">
+                                <div className={"text-center"}>
+                                    <FontAwesomeIcon icon={row.icon.fa} className={`text-xl ${row.icon.css}`} />
+                                    <div className={"text-white font-semibold"}>
+                                        {row.name}
+                                    </div>
+                                </div>
+                            </a>
+                        ))}
+                    </div>
                 </div>
             </div>
+
         )
     }
 
