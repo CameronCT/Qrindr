4d19069a01ac3b59daa58b12b9b739aa86a99fc1
Cameron <CameronCT@users.noreply.github.com>
2020-11-08 21:32:57 -0800

    feat: redesigned about page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,22 +5,25 @@ import NavigationDesktop from "./components/navigation/NavigationDesktop";
 import Match from "./components/pages/Match/Match";
 import About from "./components/pages/About/About";
 import Stream from "./components/pages/Stream/Stream";
+import Create from "./components/pages/Create/Create";
 
 export const App = () => {
   return (
       <BrowserRouter>
-        <div className="flex min-h-screen">
             <NavigationDesktop />
-            <div className="p-4 md:p-6 lg:p-12 w-full">
+            <div className={"w-10/12 mx-auto"}>
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route path="/about" component={About} />
+                    <Route path="/create" component={Create} />
                     <Route path="/match/:hash/:secret/:player" component={Match} />
                     <Route path="/match/:hash" component={Match} />
                     <Route path="/stream/:hash" component={Stream} />
                 </Switch>
             </div>
-        </div>
+            <div className={"text-center p-3 uppercase text-sm text-white font-semibold my-6"}>
+                &copy; Qrindr 2020 - Created by CameronCT
+            </div>
       </BrowserRouter>
   );
 };
--- client/src/components/pages/About/About.tsx
+++ client/src/components/pages/About/About.tsx
@@ -5,35 +5,33 @@ class About extends Component {
     render() {
         return (
             <div className={"max-w-screen-md mx-auto"}>
-                <div className={"text-center pb-10"}>
-                    <div className={"text-3xl text-white font-semibold"}>About Qrindr</div>
-                    <div className={"border-b w-48 mx-auto border-white my-6"} />
-                    <div className={"text-gray-100 text-lg"}>
+                <div className={"my-10 bg-black bg-opacity-25 p-8"}>
+                    <div className={"text-xl text-white font-semibold border-b border-gray-800 pb-3 mb-3"}>About Qrindr</div>
+                    <div className={"text-gray-100 text-sm tracking-wide"}>
                         When Quake Pro League was announced in Summer 2019, players have been emulating the formats used by professional players on their streams. For many, this is still being manually done and oftentimes leads to confusion and forgetfulness.
 
                         Qrindr is designed as a user-friendly system that walks players through both map and champion selection processes. Accounts are unnecessary and player names used are simply for show.
                     </div>
                 </div>
 
-                <div className={"text-center pt-10"}>
-                    <div className={"text-3xl text-white font-semibold"}>Frequently Asked Questions</div>
-                    <div className={"border-b w-48 mx-auto border-white my-6"} />
-                    <div className={"text-gray-100 text-lg"}>
-                        <div className={"mb-6"}>
+                <div className={"my-10 bg-black bg-opacity-25 p-8"}>
+                    <div className={"text-xl text-white font-semibold border-b border-gray-800 pb-3 mb-3"}>Frequently Asked Questions</div>
+                    <div className={"text-gray-100 text-sm tracking-wide"}>
+                        <div className={"mt-3"}>
                             <div className={"font-semibold"}>Can you add [insert map] or [insert modes] for other tournaments?</div>
                             <div>
                                 This tool was built as a way for me to easily pick my champions and maps due to the use of typing each picks out at the time was frustrating. With this in mind, the functionality is very static and is hard to modify for all modes and extra maps.
                             </div>
                         </div>
 
-                        <div className={"mb-6"}>
+                        <div className={"my-3"}>
                             <div className={"font-semibold"}>Does this app follow all "Tier 1" tournaments (ESL, FACEIT, QPL) veto standards?</div>
                             <div>
                                 While the functionality will allow most tournaments to use this system, it is ultimately up to them if they want to use them.
                             </div>
                         </div>
 
-                        <div className={"mb-6"}>
+                        <div className={"mt-3"}>
                             <div className={"font-semibold"}>What was Qrindr built with?</div>
                             <div>
                                 The app was originally built during one extremely late night and was built very poorly using PHP & AngularJS. It is now being coded with React and PHP. The code is open-sourced!
@@ -42,10 +40,9 @@ class About extends Component {
                     </div>
                 </div>
 
-                <div className={"text-center py-10"}>
-                    <div className={"text-3xl text-white font-semibold"}>Credits</div>
-                    <div className={"border-b w-48 mx-auto border-white my-6"} />
-                    <div className={"text-gray-100 text-lg"}>
+                <div className={"my-10 bg-black bg-opacity-25 p-8"}>
+                    <div className={"text-xl text-white font-semibold border-b border-gray-800 pb-3 mb-3"}>Other credits</div>
+                    <div className={"text-gray-100 text-sm tracking-wide"}>
                         I want to give a big thanks to <a className="font-semibold text-gray-400" href={"https://xjon.me"}>Jon Lachney</a> for helping with the bleep and boopity sounds for Qrindr!
                     </div>
                 </div>
